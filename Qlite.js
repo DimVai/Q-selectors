24cc7bade32cd5f9d71d32e7131718e782a743da
Dimitris Vainanidis <dvainanidis@gmail.com>
2024-06-26 19:58:39 +0300

    1.2 - Qlite

--- Qlite.js
+++ Qlite.js
@@ -12,16 +12,20 @@
 /** 
  * Returns the selected DOM elements, by ID, class, e.t.c.  
 */
-this.Q ??= (selector) => {
+window.Q = (selector) => {
     if ( selector.charAt(0)=='#' ) {  
           let element = document.querySelector(selector);    
-          element.on ??= function(event,callback){element.addEventListener(event,callback);return element}
-          element.show ??= function(condition=true){if (condition) {element.classList.remove('d-none')} else {element.classList.add('d-none')} }
+          element.on ??= function(event,callback){element.addEventListener(event,callback);return element};
+          element.set ??= function(content){element.textContent=content};
+          element.show ??= function(condition=true){if (condition) {element.classList.remove('d-none')} else {element.classList.add('d-none')} };
           return element;
     } else {
           if (selector.charAt(0)=='~') {selector=`[data-variable=${selector.substring(1)}]`}
           let elements = [...document.querySelectorAll(selector)];
-          elements.set ??= function(content){elements.forEach(el=>el.textContent=content)}
+          elements.show ??= function(condition=true){elements.forEach(el=>{
+              if (condition) {el.classList.remove('d-none')} else {el.classList.add('d-none')}
+          })};
+          elements.set ??= function(content){elements.forEach(el=>el.textContent=content)};
           elements.on ??= function(event,action,options){
               if (options=="live"){
                   document.addEventListener(event,(e)=>{if(e.target.matches(selector)){action(e)}});
